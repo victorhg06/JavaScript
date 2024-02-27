01ba56c94138b57f169dc23d39517a7f7fdd4258
victorhg06 <victorhg06@gmail.com>
2024-02-26 21:04:55 -0300

    Condição

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 55
+console.log (`Sua velocidade é ${vel} km/h`)
+if (vel > 60) {
+        console.log (`Voce ultrapassou a velocidade permitida. MULTADO!`)
+    }
+console.log(`Dirija sempre usando o cinto de segurança`)
