f6c536d77edb2e5ff9372b7d3753d1ee9b7ddef8
victorhg06 <victorhg06@gmail.com>
2024-03-01 19:25:18 -0300

    while e do while em JS

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,20 @@
+/* var c = 1
+while (c <= 10) {
+    console.log(`Número ${c}`) // Para que mostre a numeração
+    c++  // É o incremento do contador, caso eu não coloque o incremento vai rodar um loop infinito
+}
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+var c = 1;
+do {
+    console.log (`Número ${c}`)
+    c++   // Realizando o incremento aqui
+} while (c <=6)
+
---
+++ Aula 13/node_a6955f4682e1a.tmp.js
@@ -0,0 +1,20 @@
+var c = 1
+while (c < 6) {
+    console.log('Tudo bem?')
+      // É o incremento do contador
+}
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
