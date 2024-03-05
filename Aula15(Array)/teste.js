2e34a20790cd35b2a54e12b93925ec0d34b5b981
victorhg06 <victorhg06@gmail.com>
2024-03-05 20:47:46 -0300

    a

---
+++ Aula15(Array)/teste.js
@@ -0,0 +1,17 @@
+/* Ler elemento de um array
+var a = ['world']
+var value = a[0]
+console.log(value)
+*/
+
+/* array.join = converte todos os elementos de um array em strings e as concatena
+var a = [1, 2, 3]
+console.log(a.join()) //Quando nada é especificado, cria-se uma vírgula separando os elementos
+console.log(a.join("")) // Simplesmente concatenou e uniu os números
+console.log(a.join(" ")) //Acrescentei um espaço
+*/
+
+/* array.reverse = Inverte a ordem dos elementos de um array e o retorna invertido
+var a = [1, 2, 3]
+console.log(a.reverse())
+*/
