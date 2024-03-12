7d6d1b3dfecb7241c539e218c33946e063de84eb
victorhg06 <victorhg06@gmail.com>
2024-03-12 18:24:57 -0300

    Analisador de números

---
+++ Aula16 ex/modelo/script.js
@@ -0,0 +1,29 @@
+let num = document.querySelector('input#fnum') // Atribuindo 
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []  //Criação do vetor
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l){   //Essa função recebe 2 parâmetros
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else{
+        return false
+    }
+}
+
+
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+}
